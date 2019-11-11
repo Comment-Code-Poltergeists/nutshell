ec30964d993fe85aae7f74eb292d426826c8bd7e
Ken Boyd <klb417@gmail.com>
2019-11-11 15:02:20 -0600

    removed troubleshooting comments in buildDOM.js

--- src/scripts/tasks/buildDOM.js
+++ src/scripts/tasks/buildDOM.js
@@ -1,19 +1,3 @@
-/*
-
-<div id="tasks-container" class="card side-components">
-<h3>To Do</h3>
-  <article id="tasks-content" class="card-body"> Lorem Ipsumn</article>
-</div>
-
-{
-    "id": 1,
-    "userId": 1,
-    "task": "task, id: 1, userId: 1",
-    "expectedCompletionDate": "2019-04-17",
-    "isComplete": false
-},
-
-*/
 export default {
   buildTask: function(taskEntry) {
     if (taskEntry.isComplete === false) {
