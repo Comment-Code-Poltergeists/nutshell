ca5ef2a4812db0b4bca9ab4731ce6f1ebd5fc962
Sullivan Pierce <sullypierce@gmail.com>
2019-11-12 15:06:23 -0600

    added classes to news cards

--- src/scripts/articles/htmlMaker.js
+++ src/scripts/articles/htmlMaker.js
@@ -1,6 +1,6 @@
 export const makeArticleCard = (articleObj) => {
-    return `<div id="articlecard-${articleObj.userId}"
-    <h3 id="article-title-${articleObj.userId}">${articleObj.title}</h3>
+    return `<div class="card" id="articlecard-${articleObj.userId}"
+    <h3 class="card-title" id="article-title-${articleObj.userId}">${articleObj.title}</h3>
     <p id="article-url-${articleObj.userId}"><a href="${articleObj.url}">Link Here</a></p>
     <p id="article-synopsis-${articleObj.userId}">${articleObj.synopsis}</p>
     </div>`
