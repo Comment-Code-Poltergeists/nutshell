9d6277fd712a1c7ca2ec11725c31303b882e11f2
Ken Boyd <klb417@gmail.com>
2019-11-13 11:59:52 -0600

    delete task functionality added

--- src/scripts/tasks/eventListeners.js
+++ src/scripts/tasks/eventListeners.js
@@ -12,11 +12,22 @@ export const isCompleteTaskListener = () => {
 
   taskMainContainer.addEventListener("click", () => {
     const taskAction = event.target.id.split("--")[0];
-
     const taskId = event.target.id.split("--")[1];
+
     if (taskAction === "complete-task") {
       const body = { isComplete: true };
       API.patchSomething(`tasks/${taskId}`, body).then(() => {
+        API.buildYourOwnGet(`tasks/?userId=${userId}`)
+          .then(response => {
+            sessionStorage.setItem("tasks", JSON.stringify(response));
+          })
+          .then(() => {
+            renderTaskCard(JSON.parse(sessionStorage.getItem("tasks")));
+            renderMainWithListener();
+          });
+      });
+    } else if (taskAction === "delete-task") {
+      API.deleteSomething(`tasks/${taskId}`).then(() => {
         API.buildYourOwnGet(`tasks/?userId=${userId}`)
           .then(response => {
             sessionStorage.setItem("tasks", JSON.stringify(response));
