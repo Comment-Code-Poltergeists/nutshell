23ce9b37fe0143bc2ee69c31b09b6183dd6d4487
Ryan Bishop <rdbishop06@gmail.com>
2019-11-13 10:20:46 -0600

    feature: display edit btn for loggedInUser messages

--- src/scripts/messages/messagesHtmlFactory.js
+++ src/scripts/messages/messagesHtmlFactory.js
@@ -14,10 +14,17 @@ export default {
     },
     createPreviousMessageHtml(message){
         const formattedtime = convertDateTimeFromISO(message.timestamp).toLocaleString()
+        const userId = JSON.parse(sessionStorage.getItem("userId"))
+        let editButton
+
+        if (message.user.id === userId){
+            editButton = `<button id="edit-message--${message.id}" class="btn btn-light btn-sm btn-message">Edit</button>`
+        }
         return `
             <span class="message-timestamp">${formattedtime}</span>
             <span><strong>${message.user.fullName}</strong></span>
-            <p id="message--${message.id}" class="card bg-secondary border-dark">${message.message}</p>
-        `
+            <p id="message--${message.id}" class="card bg-secondary border-dark">${message.message}
+            ${editButton}</p>
+            `
     }
 }
