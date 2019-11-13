a739f19ffb129154126a1bc5c8d690a562c44b9d
Cherkesky <guy@cherkesky.com>
2019-11-13 11:01:52 -0600

    trailing spaces

--- src/scripts/events/eventListeners.js
+++ src/scripts/events/eventListeners.js
@@ -3,5 +3,5 @@ import eventManager from "./events.js"
 const eventsMainContainerRef = document.getElementById("events-container")
 
 export const eventsClickHandler=()=>{
-  eventsMainContainerRef.addEventListener("click", eventManager.displayUserEvents())
+  eventsMainContainerRef.addEventListener("click", eventManager.displayUserEvents)
 }
