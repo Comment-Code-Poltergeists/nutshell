e0f6c62b9d32e2fdb35f5137427df7d631ad6466
Ken Boyd <klb417@gmail.com>
2019-11-14 10:12:01 -0600

    sorting tasks based on date now

--- src/scripts/tasks/renderDOM.js
+++ src/scripts/tasks/renderDOM.js
@@ -1,11 +1,13 @@
 import buildDOM from "./buildDOM.js";
 import API from "../data/data.js"
+import { sortElementsByDate } from "../utilities/datetime.js";
 
 export const renderTaskCard = () => {
   const tasks = JSON.parse(sessionStorage.getItem("tasks"))
   const tasksContent = document.querySelector("#tasks-content");
+  const sortedTasks = sortElementsByDate(tasks, "expectedCompletionDate")
   tasksContent.innerHTML = "";
-  tasks.forEach(task => {
+  sortedTasks.forEach(task => {
     if (task.isComplete === false) {
       tasksContent.appendChild(buildDOM.buildTaskCard(task));
     }
@@ -15,12 +17,14 @@ export const renderTaskCard = () => {
 export const renderTaskMain = () => {
   const tasks = JSON.parse(sessionStorage.getItem("tasks"))
   const mainContent = document.querySelector("#main-container");
+  const sortedTasks = sortElementsByDate(tasks, "expectedCompletionDate")
+
   mainContent.innerHTML = "<h1>All Tasks</h1>";
 
   const taskMain = document.createElement("article");
   taskMain.id = "task-main";
 
-  tasks.forEach(task => {
+  sortedTasks.forEach(task => {
     if (task.isComplete === false) {
       taskMain.appendChild(buildDOM.buildTaskMain(task));
     }
