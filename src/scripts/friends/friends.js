39167c3af13d34d3c0de3f9020d9e6a45c82a0c5
Chase Fite <chasef7031@gmail.com>
2019-11-15 09:50:27 -0600

    fixed syntax error for friendsEventListerners module

--- src/scripts/friends/friends.js
+++ src/scripts/friends/friends.js
@@ -4,7 +4,7 @@ Author: Chase Fite
  */
 
 import { displaySideFriendsList } from "./friendsDisplay.js"
-import { clickFriendsSideContainer, addSearchFunction } from "./friendsEventListeners.js/index.js"
+import { clickFriendsSideContainer, addSearchFunction } from "./friendsEventListeners.js"
 
 // container for all "friends" functionality
 export const displayFriends = () => {
