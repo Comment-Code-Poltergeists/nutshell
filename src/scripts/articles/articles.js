885c4aa70996b7775e9ca45461a21ed9a2359335
Sullivan Pierce <sullypierce@gmail.com>
2019-11-15 11:09:25 -0600

    working on load

--- src/scripts/articles/articles.js
+++ src/scripts/articles/articles.js
@@ -1,26 +1,49 @@
 //Author: Sully, Purpose: putting everything together for one function to be called in main
 import { makeArticleCard } from "./htmlMaker";
 import { articlesEventListener, addMainEventListener } from "./eventListeners";
-import {userId} from "../main.js"
+import { userId } from "../main.js"
 import API from "../data/data.js"
 import { populateArticlesToMain } from "./ArticlesToMain";
 
 export const populateArticleModule = () => {
-const containerRef = document.getElementById("articles-content")
-containerRef.innerHTML = ""
-let data = JSON.parse(sessionStorage.getItem("articles"))
-data.forEach(element => {
-    const newArt = makeArticleCard(element);
-    containerRef.innerHTML += newArt
-    const cardRef = document.getElementById(`articlecard-${element.id}`)
-    if (element.userId === userId) {
-        cardRef.classList.add("bg-secondary")
+    const containerRef = document.getElementById("articles-content")
+    containerRef.innerHTML = ""
+    if (JSON.parse(sessionStorage.getItem("articles")) !== null) {
+        let data = JSON.parse(sessionStorage.getItem("articles"))
+        data.forEach(element => {
+            const newArt = makeArticleCard(element);
+            containerRef.innerHTML += newArt
+            const cardRef = document.getElementById(`articlecard-${element.id}`)
+            if (element.userId === userId) {
+                cardRef.classList.add("bg-secondary")
+            } else {
+                cardRef.classList.add("csbg")
+
+            }
+        });
     } else {
-        cardRef.classList.add("csbg")
-        
+        console.log("nothing in session storage!")
+        buildYourOwnGet("friends").then((friendsList) => {
+            let Url = `articles?userId=${userId}`;
+            friendsList.forEach(element => {
+                Url += `&userId=${element.user.id}`
+            });
+            API.buildYourOwnGet(Url).then((articlesArray) => {
+                sessionStorage.setItem("articles", JSON.stringify(articlesArray));
+                articlesArray.forEach(element => {
+                    const newArt = makeArticleCard(element);
+                    containerRef.innerHTML += newArt
+                    const cardRef = document.getElementById(`articlecard-${element.id}`)
+                    if (element.userId === userId) {
+                        cardRef.classList.add("bg-secondary")
+                    } else {
+                        cardRef.classList.add("csbg")
+                    }
+                })
+            })
+            articlesEventListener()
+        })
     }
-});
-articlesEventListener();
 }
 
 //update the articles shown on the dom in both main and the articles section
@@ -31,7 +54,7 @@ export const updateDomArticles = () => {
         Url += `&userId=${element.user.id}`
     });
     API.buildYourOwnGet(Url).then((articlesArray) => {
-        sessionStorage.setItem("articles",JSON.stringify(articlesArray));
+        sessionStorage.setItem("articles", JSON.stringify(articlesArray));
         populateArticleModule();
         populateArticlesToMain();
     })
